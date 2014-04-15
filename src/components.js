3eb12798fdc655067a612bc8bfb1024b6e46980a
Noah Muth <noahmuth@gmail.com>
2014-04-15 17:38:38 -0500

    Set up island generation, quadrant-based tiles.

--- src/components.js
+++ src/components.js
@@ -34,6 +34,9 @@ Crafty.c('Grid', {
 		if (x === undefined && y === undefined) {
 			return Game.toGrid(this.x, this.y);
 		} else {
+			if (x >= 0 && y >= 0
+				&& x < Game.map_grid.width
+				&& y < Game.map_grid.height)
 			this.attr({
 				x: Game.findX(x),
 				y: Game.findY(y)
@@ -44,9 +47,130 @@ Crafty.c('Grid', {
 	}
 });
 
+Crafty.c('MapObject', {
+	init: function() {
+		this.requires('2D, Canvas, Grid, Mouse')
+			.bind('Click', this.clickHandler);
+	},
+
+	clickHandler: function(e) {
+		if (e.mouseButton == Crafty.mouseButtons.LEFT) {
+			console.log(this.at().x, this.at().y,
+					Game.neighbors(this.at().x, this.at().y));
+		}
+	}
+});
+
+Crafty.c('MapQuad', {
+	init: function() {
+		this.requires('Grid');
+
+		this.quadrants = {
+			upLeft: null,
+			upRight: null,
+			downLeft: null,
+			downRight: null
+		};
+
+		this.quadrantCoords = {
+			upLeft: {
+				x: this.x,
+				y: this.y
+			},
+			upRight: {
+				x: this.x + (this.w/2),
+				y: this.y
+			},
+			downLeft: {
+				x: this.x,
+				y: this.y + (this.h/2)
+			},
+			downRight: {
+				x: this.x + (this.w/2),
+				y: this.y + (this.h/2)
+			}
+		}
+	},
+
+	// TODO: Change these methods to take in a component list and use that to create the object,
+	//		rather than taking in an actual object.
+	setQuadrants: function(obj) {
+		var toCreate = {};
+		toCreate.upLeft = obj.upLeft? obj.upLeft : this.quadrants.upLeft;
+		toCreate.upRight = obj.upRight? obj.upRight : this.quadrants.upRight;
+		toCreate.downLeft = obj.downLeft? obj.downLeft : this.quadrants.downLeft;
+		toCreate.downRight = obj.downRight? obj.downRight : this.quadrants.downRight;
+
+		for (key in toCreate) {
+			this.quadrants[key] = Crafty.e(toCreate[key]);
+		}
+
+		return this.enforceQuadrantRestrictions();
+	},
+
+	setAllQuadrants: function(obj) {
+		for (key in this.quadrants) {
+			this.quadrants[key] = obj;
+		}
+
+		return this.enforceQuadrantRestrictions();
+	},
+
+	enforceQuadrantRestrictions: function() {
+		this.quadrantCoords = {
+			upLeft: {
+				x: this.x,
+				y: this.y
+			},
+			upRight: {
+				x: this.x + (Game.map_grid.tile.width/2),
+				y: this.y
+			},
+			downLeft: {
+				x: this.x,
+				y: this.y + (Game.map_grid.tile.height/2)
+			},
+			downRight: {
+				x: this.x + (Game.map_grid.tile.width/2),
+				y: this.y + (Game.map_grid.tile.height/2)
+			}
+		}
+
+		// console.log("before", this.quadrants);
+		for (key in this.quadrants) {
+			// console.log(key);
+
+			var q = this.quadrants[key];
+
+			if (!q || !q.has || !q.has('MapObject')) {
+				this.quadrants[key] = null;
+			} else {
+				// console.log("coords", key, this.quadrantCoords, this.quadrantCoords[key]);
+				q.attr(this.quadrantCoords[key]);
+				q.attr({
+					w: this.w/2,
+					h: this.h/2
+				});
+			}
+		}
+		// console.log("after", this.quadrants);
+
+		/* test input:
+		var asdf = Crafty.e('MapQuad').at(1,1).setQuadrants({
+			upLeft: Crafty.e('Sand'),
+			upRight: Crafty.e('Grass'),
+			downLeft: Crafty.e('Rock'),
+			downRight: Crafty.e('Island')
+		});
+		*/
+
+		return this;
+	}
+});
+
 Crafty.c('Actor', {
 	init: function() {
-		this.requires('2D, Canvas, Grid, Tween');
+		this.requires('MapObject, Tween');
 
 		this.attr({
 			speed: 200,
@@ -95,13 +219,27 @@ Crafty.c('Actor', {
 
 Crafty.c('Rock', {
 	init: function() {
-		this.requires('Actor, spr_rock, Solid');
+		this.requires('MapObject, spr_rock, Solid');
 	}
 });
 
 Crafty.c('Island', {
 	init: function() {
-		this.requires('Actor, spr_island, Solid');
+		this.requires('MapObject, spr_island, Solid');
+	}
+});
+
+Crafty.c('Grass', {
+	init: function() {
+		this.requires('MapObject, Color')
+			.color('rgb(100, 255, 100)');
+	}
+});
+
+Crafty.c('Sand', {
+	init: function() {
+		this.requires('MapObject, Color')
+			.color('#EDE291');
 	}
 });
 
@@ -221,6 +359,8 @@ Crafty.c('PlayerCharacter', {
 
 	checkSquare: function() {
 		var objArray = Game.mapObjects[this.at().x][this.at().y];
+
+		console.log(objArray);
 
 		for (var i=0; i<objArray.length; i++) {
 			if (objArray[i].has('Port')) {
--- src/scenes.js
+++ src/scenes.js
@@ -1,3 +1,150 @@
+function generateMap() {
+	var grassTiles = [];
+	var sandTiles = [];
+	var shallowWaterTiles = [];
+
+	// mark the middle third of the map as land
+	var startX = Math.floor(Game.map_grid.width/3);
+	var endX = startX*2;
+	var startY = Math.floor(Game.map_grid.height/3);
+	var endY = startY*2;
+
+	for (var x=startX; x<endX; x++) {
+		for (var y=startY; y<endY; y++) {
+			var tile = Crafty.e('Grass').at(x, y);
+			Game.addObject(tile);
+			grassTiles.push(tile);
+		}
+	}
+
+	// Now travel outwards in a circle
+	var spirals = 4;
+	var counter = 0;
+	var landChanceBonusPerSurroundingTile = 1/8;
+	var landThreshold = .65;
+
+	for (var counter = 0; counter < spirals; counter++) {
+		startX--;
+		startY--;
+
+		var landChance = 0;
+		landChanceBonusPerSurroundingTile *= .95;
+
+		for (var x = startX; x < endX; x++) {
+			landChance = Math.random()-Math.random();
+			Game.forEachNeighbor(x, startY, function(neighbor) {
+				if (neighbor !== 'null' && neighbor.has('Grass'))
+					landChance += landChanceBonusPerSurroundingTile;
+			});
+
+			if (landChance > landThreshold) {
+				var tile = Crafty.e('Grass').at(x, startY);
+				Game.addObject(tile);
+				grassTiles.push(tile);
+			}
+		}
+
+		for (var y = startY; y < endY; y++) {
+			landChance = Math.random();
+			Game.forEachNeighbor(endX, y, function(neighbor) {
+				if (neighbor !== 'null' && neighbor.has('Grass'))
+					landChance += landChanceBonusPerSurroundingTile;
+			});
+
+			if (landChance > landThreshold) {
+				var tile = Crafty.e('Grass').at(endX,y);
+				Game.addObject(tile);
+				grassTiles.push(tile);
+			}
+		}
+
+		for (var x = endX; x > startX; x--) {
+			landChance = Math.random();
+			Game.forEachNeighbor(x, endY, function(neighbor) {
+				if (neighbor !== 'null' && neighbor.has('Grass'))
+					landChance += landChanceBonusPerSurroundingTile;
+			});
+
+			if (landChance > landThreshold) {
+				var tile = Crafty.e('Grass').at(x,endY);
+				Game.addObject(tile);
+				grassTiles.push(tile);
+			}
+		}
+
+		for (var y = endY; y > startY; y--) {
+			landChance = Math.random();
+			Game.forEachNeighbor(startX, y, function(neighbor) {
+				if (neighbor !== 'null' && neighbor.has('Grass'))
+					landChance += landChanceBonusPerSurroundingTile;
+			});
+
+			if (landChance > landThreshold) {
+				var tile = Crafty.e('Grass').at(startX,y);
+				Game.addObject(tile);
+				grassTiles.push(tile);
+			}
+		}
+
+		endX++;
+		endY++;
+	}
+
+	// mark all land tiles touching water to be sand
+	for (var i = 0; i < grassTiles.length; i++) {
+		var touchingWater = false;
+
+		var quads = {
+			upLeft: 'Grass',
+			upRight: 'Grass',
+			downLeft: 'Grass',
+			downRight: 'Grass'
+		};
+
+		Game.forEachNeighbor(grassTiles[i].at().x, grassTiles[i].at().y, function(neighbor, key, index) {
+			if (neighbor === 'null') {
+
+				switch (key) {
+					case 'upLeft':
+						quads.upLeft = 'Sand';
+						break;
+					case 'up':
+						quads.upLeft = 'Sand';
+						quads.upRight = 'Sand';
+						break;
+					case 'upRight':
+						quads.upRight = 'Sand';
+						break;
+					case 'left':
+						quads.upLeft = 'Sand';
+						quads.downLeft = 'Sand';
+						break;
+					case 'right':
+						quads.upRight = 'Sand';
+						quads.downRight = 'Sand';
+						break;
+					case 'downLeft':
+						quads.downLeft = 'Sand';
+						break;
+					case 'down':
+						quads.downLeft = 'Sand';
+						quads.downRight = 'Sand';
+						break;
+					case 'downRight':
+						quads.downRight = 'Sand';
+						break;
+				}
+			}
+		}, true);
+
+		Game.addObject(Crafty.e('MapQuad')
+				.at(grassTiles[i].at().x, grassTiles[i].at().y)
+				.setQuadrants(quads));
+
+		grassTiles[i].destroy();
+	}
+}
+
 Crafty.scene('Game', function() {
 	this.occupied = new Array(Game.map_grid.width);
 	Game.mapObjects = new Array(Game.map_grid.width);
@@ -10,47 +157,49 @@ Crafty.scene('Game', function() {
 		}
 	}
 
-	this.player = Crafty.e('PlayerCharacter').at(10, 10);
-	//console.log("PLACED AT: " + this.player.at().x + ", " + this.player.at().y);
-	//console.log("ACTUAL: " + this.player.x + ", " + this.player.y);
-	this.occupied[this.player.at().x][this.player.at().y] = true;
+	generateMap();
+
+	this.player = Crafty.e('PlayerCharacter').at(0, 0);
+	// //console.log("PLACED AT: " + this.player.at().x + ", " + this.player.at().y);
+	// //console.log("ACTUAL: " + this.player.x + ", " + this.player.y);
+	// this.occupied[this.player.at().x][this.player.at().y] = true;
 	
-	this.enemy = Crafty.e('Enemy').at(Game.map_grid.width-2, Game.map_grid.height-2);
-	this.occupied[this.enemy.at().x][this.enemy.at().y] = true;
+	// this.enemy = Crafty.e('Enemy').at(Game.map_grid.width-2, Game.map_grid.height-2);
+	// this.occupied[this.enemy.at().x][this.enemy.at().y] = true;
 
-	for (var x=0; x<Game.map_grid.width; x++) {
-		for (var y=0; y<Game.map_grid.height; y++) {
-			var at_edge = x == 0 || x == Game.map_grid.width-1
-				|| y == 0 || y == Game.map_grid.height-1;
+	// for (var x=0; x<Game.map_grid.width; x++) {
+	// 	for (var y=0; y<Game.map_grid.height; y++) {
+	// 		var at_edge = x == 0 || x == Game.map_grid.width-1
+	// 			|| y == 0 || y == Game.map_grid.height-1;
 
-			if (at_edge) {
-				Game.mapObjects[x][y].push(Crafty.e('Rock').at(x, y).toggleMargin());
-				this.occupied[x][y] = true;
-			} else if (!this.occupied[x][y] && Math.random() < 0.06) {
-				// Place a bush entity at the current tile
-				Game.mapObjects[x][y].push(Crafty.e('Island').at(x, y));
-				this.occupied[x][y] = true;
-			}
-		}
-	}
+	// 		if (at_edge) {
+	// 			Game.mapObjects[x][y].push(Crafty.e('Rock').at(x, y).toggleMargin());
+	// 			this.occupied[x][y] = true;
+	// 		} else if (!this.occupied[x][y] && Math.random() < 0.06) {
+	// 			// Place a bush entity at the current tile
+	// 			Game.mapObjects[x][y].push(Crafty.e('Island').at(x, y));
+	// 			this.occupied[x][y] = true;
+	// 		}
+	// 	}
+	// }
 
-	var maxPorts = 5;
+	// var maxPorts = 5;
 
-	while (Crafty('Port').length < maxPorts) {
-		var x = Math.floor(Math.random() * Game.map_grid.width);
-		var y = Math.floor(Math.random() * Game.map_grid.height);
+	// while (Crafty('Port').length < maxPorts) {
+	// 	var x = Math.floor(Math.random() * Game.map_grid.width);
+	// 	var y = Math.floor(Math.random() * Game.map_grid.height);
 
-		if (this.occupied[x][y]) {
-			continue;
-		} else {
-			try {
-				Game.mapObjects[x][y].push(Crafty.e('Port').at(x, y));
-				this.occupied[x][y] = true;
-			} catch(err) {
-				console.log(x, y);
-			}
-		}
-	}
+	// 	if (this.occupied[x][y]) {
+	// 		continue;
+	// 	} else {
+	// 		try {
+	// 			Game.mapObjects[x][y].push(Crafty.e('Port').at(x, y));
+	// 			this.occupied[x][y] = true;
+	// 		} catch(err) {
+	// 			console.log(x, y);
+	// 		}
+	// 	}
+	// }
 
 	this.showVictory = this.bind('PortVisited', function() {
 		if (!Crafty('Port').length) {
